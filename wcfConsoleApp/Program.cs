8e4b0003c184aac8548bbe332819d8c807ff36fa
lewisaudeone <80711658+lewisc99@users.noreply.github.com>
2024-06-02 13:04:43 -0300

    wcf with raw code init project

---
+++ wcfConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using wcfConsoleApp.Services;
+
+namespace wcfConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the ServiceHost for the MyService type.
+            using (ServiceHost host = new ServiceHost(typeof(MyService)))
+            {
+                // Create bindings
+                WSHttpBinding wsBinding = new WSHttpBinding();
+                NetTcpBinding tcpBinding = new NetTcpBinding();
+
+                // Add service endpoints
+                host.AddServiceEndpoint(typeof(IMyContract), wsBinding, "http://localhost:8000/MyService");
+                //host.AddServiceEndpoint(typeof(IMyContract), tcpBinding, "net.tcp://localhost:8001/MyService");
+                //host.AddServiceEndpoint(typeof(IMyOtherContract), tcpBinding, "net.tcp://localhost:8002/MyService");
+
+                // Enable metadata exchange
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+                host.Description.Behaviors.Add(smb);
+
+                // Open the ServiceHost to start listening for messages.
+                host.Open();
+
+                Console.WriteLine("The service is ready at http://localhost:8000/MyService");
+                //Console.WriteLine("The service is ready at net.tcp://localhost:8001/MyService");
+                //Console.WriteLine("The service is ready at net.tcp://localhost:8002/MyService");
+                Console.WriteLine("Press <Enter> to stop the service.");
+                Console.ReadLine();
+
+                // Close the ServiceHost.
+                host.Close();
+            }
+        }
+    }
+}
---
+++ wcfConsoleApp/Services/IMyContract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+
+namespace wcfConsoleApp.Services
+{
+    [ServiceContract]
+    public interface IMyContract
+    {
+        [OperationContract]
+        string MyMethod(string data);
+    }
+
+    [ServiceContract]
+    public interface IMyOtherContract
+    {
+        [OperationContract]
+        int AnotherMethod(int number);
+    }
+}
