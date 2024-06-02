ec85f6133e0a63e13e8ff4c4c85aa1aec1de83a0
lewisaudeone <80711658+lewisc99@users.noreply.github.com>
2024-06-02 16:46:05 -0300

    change wcf project names

---
+++ 05_wcfConsoleApp_custom/wcfConsoleApp/Services/MyService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wcfConsoleApp.Services
+{
+    public class MyService : IMyContract, IMyOtherContract
+    {
+        public string MyMethod(string data)
+        {
+            return $"You sent: {data}";
+        }
+
+        public int AnotherMethod(int number)
+        {
+            return number * 2;
+        }
+
+        public static void Configure(ServiceConfiguration config)
+        {
+            Binding wsBinding = new WSHttpBinding();
+            config.AddServiceEndpoint(typeof(IMyContract), wsBinding,
+            new Uri("http://localhost:8001/MyService"));
+        }
+
+    }
+}
