ce3bbd208a4c38ef3f5fbb4b07b3095006d8f762
lewisaudeone <80711658+lewisc99@users.noreply.github.com>
2024-06-02 14:40:46 -0300

    added config directly in the implementation service in Wcf

--- wcfConsoleApp/wcfConsoleApp/Services/MyService.cs
+++ wcfConsoleApp/wcfConsoleApp/Services/MyService.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,5 +20,13 @@ namespace wcfConsoleApp.Services
         {
             return number * 2;
         }
+
+        public static void Configure(ServiceConfiguration config)
+        {
+            Binding wsBinding = new WSHttpBinding();
+            config.AddServiceEndpoint(typeof(IMyContract), wsBinding,
+            new Uri("http://localhost:8001/MyService"));
+        }
+
     }
 }
